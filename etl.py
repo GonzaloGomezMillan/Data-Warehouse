d74c7df59228405a28b3dae569f9493cdb75653c
Gonzalo Gomez Millan del Villar <gonzalogomezmillandelvillar@gmail.com>
2022-12-03 10:22:21 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,6 +4,9 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    This function load the staging table to S3, so they can transformed to be loaded into Redshift.
+    '''
     for query in copy_table_queries:
         print(query)
         cur.execute(query)
@@ -11,6 +14,10 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    '''
+    This function load the data stored in the staging tables into the tables which has been already created for the DWH
+    in Redshift.
+    '''
     for query in insert_table_queries:
         print(query)
         cur.execute(query)
@@ -18,6 +25,10 @@ def insert_tables(cur, conn):
 
 
 def main():
+    '''
+    Function which runs the complete code to load the data into the staging tables and afterwards to load this data into
+    the Redshift DWH.
+    '''
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
